aa92a7c8292c2c85165cecbc951b3ef222104f49
dorra ben abdelwahed <ben-abdelwahed.dorra@wimova.com>
2023-05-07 16:53:15 +0100

    Initial Commit

---
+++ Quotes_App/Quotes_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quotes_AppApp.swift
+//  Quotes_App
+//
+//  Created by dorra ben abdelwahed on 7/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct Quotes_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
